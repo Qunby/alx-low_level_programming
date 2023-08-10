87e55aaee114bda54875d78d740194a84486b111
Qunby <Qunbie_27@yahoo.com>
2023-08-10 11:25:29 -0700

    programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,12 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{       int a;long int b;long long int c;char d;float f;
-	printf("size of a char: %zu byte(s)\n", sizeof(d));
-        printf("size of an int: %zu byte(s)\n", sizeof(a));
-	printf("size of a long int: %zu byte(s)\n", sizeof(b));
-	printf("size of a long long: %zu byte(s)\n", sizeof(c));
-	printf("size of a float: %zu byte(s)\n", sizeof(f));
-	return (0);
+{
+int a;long int b;long long int c;char d;float f;
+printf("size of a char: %zu byte(s)\n", sizeof(d));
+printf("size of an int: %zu byte(s)\n", sizeof(a));
+printf("size of a long int: %zu byte(s)\n", sizeof(b));
+printf("size of a long long: %zu byte(s)\n", sizeof(c));
+printf("size of a float: %zu byte(s)\n", sizeof(f));
+return (0);
 }
