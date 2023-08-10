1a2d2981f7bc17372a9cda9a4959d8d8884163bd
Qunby <Qunbie_27@yahoo.com>
2023-08-10 08:37:29 -0700

    programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * Main - entry point
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle\n");
+	return 0;
+}
+
