9b2d18d0c007705b9eb05d2172b1053d9bc1c7d0
Qunby <Qunbie_27@yahoo.com>
2023-08-10 11:33:14 -0700

    programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,10 @@
  */
 int main(void)
 {
-int a;long int b;long long int c;char d;float f;
-printf("size of a char: %zu byte(s)\n", sizeof(d));
-printf("size of an int: %zu byte(s)\n", sizeof(a));
-printf("size of a long int: %zu byte(s)\n", sizeof(b));
-printf("size of a long long: %zu byte(s)\n", sizeof(c));
-printf("size of a float: %zu byte(s)\n", sizeof(f));
-return (0);
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+        printf("size of an int: %zu byte(s)\n", sizeof(int));
+        printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+        printf("size of a long long: %zu byte(s)\n", sizeof(long long int));
+        printf("size of a float: %zu byte(s)\n", sizeof(float));
+        return (0);
 }
