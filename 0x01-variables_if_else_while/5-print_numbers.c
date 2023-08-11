9c51b22905d1ef9a241bb54b8125a784b95e5530
Qunby <Qunbie_27@yahoo.com>
2023-08-11 09:09:13 -0700

    variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int g;
+
+	for (g = 0; g < 10; g++)
+	{
+		printf("%d", g);
+	}
+	putchar('\n');
+	return (0);
+}
