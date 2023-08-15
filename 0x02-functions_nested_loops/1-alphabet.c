e81f36ebe75a36a719e7be6cdf2c907447faa748
Qunby <Qunbie_27@yahoo.com>
2023-08-15 09:43:37 -0700

    functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		_putchar("b");
+	}
+	_putchar("\n");
+}
