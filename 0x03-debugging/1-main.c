2d9dc2cc7330d479d92bbb2708de6c3a32cf353b
Qunby <Qunbie_27@yahoo.com>
2023-08-17 08:26:33 -0700

    debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * Return: Always 0 (Success)
+ * Return: 0
  */
 
 int main(void)
@@ -13,13 +13,12 @@ int main(void)
 
 	i = 0;
 
-	/* while (i < 10)*/
+	/*while (i < 10)*/
 	/*{*/
-	/* putchar (i);*/
+		/*putchar(i);*/
 	/*}*/
 
 	printf("infinite loop avoided \\o/\n");
 
 	return (0);
 }
-
