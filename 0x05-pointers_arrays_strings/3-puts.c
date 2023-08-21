d001db3a0e37fb635b3a67e839858c899befb3ee
Qunby <Qunbie_27@yahoo.com>
2023-08-21 10:14:30 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints character strings
+ * @str: the string
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
