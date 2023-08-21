7fd731d3324878da5246d7e1e00785c5076036b1
Qunby <Qunbie_27@yahoo.com>
2023-08-21 15:45:39 -0700

    arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,14 +9,14 @@ int _atoi(char *s)
 {
 	int x = 0;
 	unsigned int ni = 0;
-	int mn = 1;
+	int min = 1;
 	int isi = 0;
 
 	while (s[x])
 	{
 		if (s[x] == 45)
 		{
-			mn *= -1;
+			min *= -1;
 		}
 
 		while (s[x] >= 48 && s[x] <= 57)
@@ -34,6 +34,6 @@ int _atoi(char *s)
 		x++;
 	}
 
-	ni = mn;
+	ni *= min;
 	return (ni);
 }
