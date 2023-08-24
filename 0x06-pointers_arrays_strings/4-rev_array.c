0f76a74e753a9c3d0f7a633932229b524f7d9f85
Qunby <Qunbie_27@yahoo.com>
2023-08-24 09:53:41 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reverse_array - reversing array
+ * @a - an array of integer
+ * @n: Number of element of the array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int d, f;
+
+	for (d = 0; (d < (n - 1) / 2); d++)
+	{
+		f = a[d];
+		a[d] = a[n - 1 - d];
+		a[n - 1 - d] = f;
+	}
+}
