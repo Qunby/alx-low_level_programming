dcd16418e01975df4f4068874e9ff90137606ffc
Qunby <Qunbie_27@yahoo.com>
2023-08-24 15:51:30 -0700

    advanced

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,4 @@
 #include <stdio.h>
-
-/**
- * main - entry point
- * Return:Always 0
- */
 int main(void)
 {
 	int n;
@@ -14,7 +9,7 @@ int main(void)
 	p = &n;
 	/*
 	 * write your line of code here...
-	 * Remember:
+i	 * Remember:
 	 * - you are not allowed to use a
 	 *   - you are not allowed to modify p
 	 *   - only one statement
