cbd9b2684410e923b081b21530c58ca8c9a156ee
Qunby <Qunbie_27@yahoo.com>
2023-09-13 11:08:33 +0100

    functions to pointers

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * get_op_func - ...
+  * @s: ...
+  *
+  * Return: ...
+  */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+	{ "+", op_add },
+	{ "-", op_sub },
+	{ "*", op_mul },
+	{ "/", op_div },
+	{ "%", op_mod },
+	{ NULL, NULL }
+	};
+	int fig = 0;
+
+	while (fig < 5)
+	{
+		if (strcmp(s, ops[fig].op) == 0)
+			return (ops[fig].f);
+
+		fig++;
+	}
+
+	return (0);
+}
