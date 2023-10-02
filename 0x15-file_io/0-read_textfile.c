5728b4fdf444d06769a3aa4b0315181b094d2d3a
Qunby <Qunbie_27@yahoo.com>
2023-10-03 00:38:12 +0100

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buff == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY, 0600);
+	fd = open(filename, O_RDWR);
 
 	if (fd == -1)
 	{
