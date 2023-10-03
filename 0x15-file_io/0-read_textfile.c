b51f09a2eefd985d2c01dbef79414c9c0e813d71
Qunby <Qunbie_27@yahoo.com>
2023-10-03 01:42:02 +0100

    task update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,38 +9,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    if (filename == NULL)
-        return 0;
+    ssize_t o, r, w;
+	char *buffer;
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return 0;
+	if (filename == NULL)
+		return (0);
 
-    char *buffer = malloc(sizeof(char) * (letters + 1));
-    if (buffer == NULL)
-    {
-        close(fd);
-        return 0;
-    }
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+		return (0);
 
-    ssize_t bytes_read = read(fd, buffer, letters);
-    if (bytes_read == -1)
-    {
-        close(fd);
-        free(buffer);
-        return 0;
-    }
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-    ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read)
-    {
-        close(fd);
-        free(buffer);
-        return 0;
-    }
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    close(fd);
-    free(buffer);
+	free(buffer);
+	close(o);
 
-    return bytes_written;
+	return (w);
 }
